c95c8372c61dcaf9fd28d5c79a5f7beb516579c7
Emily <yday1223@gmail.com>
2022-02-15 20:49:53 +0900

    make-button

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+'use strict';
+
 const loginForm = document.querySelector('#loginForm');
 const loginPage = document.querySelector('.login');
 const mainPage = document.querySelector('.main');
 const myName = document.querySelector('.inputName');
 const greeting = document.querySelector('.greeting');
 
-function openMain(event) {
-  event.preventDefault();
-  loginPage.classList.add('hidden');
-  greeting.innerText = `Have a good day ${myName.value}`;
-  mainPage.classList.remove('hidden');
+// // go to Main page
+// function openMain(event) {
+//   event.preventDefault();
+//   loginPage.classList.add('hidden');
+//   greeting.innerText = `Have a good day ${myName.value}`;
+//   mainPage.classList.remove('hidden');
+// }
+
+// loginForm.addEventListener('submit', openMain);
+
+// change screen
+const buttons = document.querySelector('.buttons');
+const screen = document.querySelector('.screen');
+function handleScreen(event) {
+  const subject = event.target.id;
+  if (subject === 'calculator') {
+    screen.style.backgroundColor = 'red';
+  } else if (subject === 'map') {
+    screen.style.backgroundColor = 'orange';
+  } else if (subject === 'weather') {
+    screen.style.backgroundColor = 'green';
+  } else if (subject === 'memo') {
+    screen.style.backgroundColor = 'blue';
+  }
 }
 
-loginForm.addEventListener('submit', openMain);
+buttons.addEventListener('click', handleScreen);
